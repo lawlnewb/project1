c7507eb16c92193230581b8912e88daea87461a3
lawlnewb <lawlnewb@gmail.com>
2016-03-22 19:39:37 -0700

    Added intro paragraph

--- run.rb
+++ run.rb
@@ -2,9 +2,16 @@
 # Project1
 #
 
-def Start
-    puts "You find yourself in a room"
+def start
+    story1 = 'You are Colm, a knight from the kingdom of Galveria. '\
+    'At a mere 18 years old, you managed to climb your way up from the '\
+    'ranks of a mere foot soldier to ranks of the Knights of the Iron '\
+    'Cross. As a new member, King Magnus has sent you to investigate '\
+    'reports of missing people from the village of Renovert. It has been '\
+    'seven days since you left the capital. You have just arrived at the '\
+    'village gates.'
 
+    puts story1
 
 end
 
@@ -12,7 +19,7 @@ end
 
 
 if __FILE__ == $0
-    puts "Hello World"
+    start
 
 
 end
